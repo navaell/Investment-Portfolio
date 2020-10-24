8829efb99678815cb0b9d91b481b4f5fe3033eb0
Nadezhda Dimitrova <nadya.dimitrova257@gmail.com>
2020-10-24 19:02:19 +0100

    connection made with database

---
+++ src/test/java/com/github/navaell/investmentportfolio/InvestmentPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.navaell.investmentportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
